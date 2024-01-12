0f4e9bb3199ec5c9fcd06810f1189d4bf754fafb
Srini Pandiyan <srinipandiyan7@gmail.com>
2024-01-11 19:09:39 -0600

    template with fixes

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { render, fireEvent } from "@testing-library/react";
 import Card from "./Card.js";
-import TEST_IMAGES from "./_testCommon.js";
+//import TEST_IMAGES from "./_testCommon.js";
 
 
 test('it renders without crashing', () => {
-  render(<App />);
+  render(<Card
+      caption='card-title-test'
+      src='image-src-test'
+      currNum={1}
+      totalNum={3}
+    />
+  );
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,6 +2,6 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
-//Maybe remove '/extend-expect' if testing is an issue
+//Maybe add/remove '/extend-expect' if testing is an issue
